a178e22114e455b061b96ee1ded8a2fd4c5798c8
chriscools1 <chris@pomax.com>
2018-12-17 20:35:52 +0100

    gedaan tijdens de les

--- Les 13/les 13 test.py
+++ Les 13/les 13 test.py
@@ -1,8 +1,32 @@
 
-from urllib2 import urlopen
+from urllib import urlopen
+from bs4 import BeautifulSoup
 
-url = 'https://scrapebook22.appspot.com'
+
+url = 'https://scrapebook22.appspot.com/'
 response = urlopen(url).read()
-print response
+soup = BeautifulSoup(response, 'html.parser')
+print(soup.html.head.title.string)
+
+names = []
+gender = []
+ages = []
+cities = []
+emails = []
+
+data = {""}
 
 
+for link in soup.findAll("a"):
+    if link.string == "See full profile":
+        person_url = url[:-1] + link["href"]
+        #print person_url
+        person_html = urlopen(person_url).read()
+        person_soup = BeautifulSoup(person_html, 'html.parser')
+        for h1 in person_soup.findAll("h1"):
+            if h1.string != "Hello, ninja!":
+                name = h1.string
+                print name
+                names.append(h1.string)
+        print person_soup.find("span", attrs={"class": "email"}).string
+        emails.append(person_soup.find("span", attrs={"class": "email"}).string)
